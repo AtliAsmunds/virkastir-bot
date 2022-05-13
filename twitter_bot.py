f2bc657313097e1c6c8e60248202d31ebd14e329
AtliAsmunds <atliasmunds@gmail.com>
2022-05-13 15:59:49 +0000

    make twitter bot post to twitter

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
-from utils.fb_scraper import CommentScraper
+from utils.fb_scraper import CommentScraper, NoDataError, User
+from random import sample
 import os
 import json
 import tweepy
 
+MAX_POST_LEN = 280
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 CONFIG_FILE = os.path.join(BASEDIR, 'utils', 'scrape_config.json')
 
@@ -15,8 +17,24 @@ access_secret = os.getenv('TWITTER_SECRET_ACCESS')
 
 client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_key, access_token_secret=access_secret)
 
+
+
 with open(CONFIG_FILE) as f:
     config = json.load(f) 
 
 scraper = CommentScraper(config)
 scraper.scrape()
+top_commenter: User = scraper.get_top_commenters(top=1)[0]
+
+text = f"Fjöldi pósta: {scraper.get_nr_comments()}\n\nVirkasti notandinn er {top_commenter} með {len(top_commenter)} comment/reply\n\n"
+
+random_comments = sample(top_commenter.get_comments(), len(top_commenter))
+
+for comment in random_comments:
+    if len(comment[1]) + len(text) + 2 <= MAX_POST_LEN:
+        text += f'"{comment[1]}"'
+        break
+
+
+
+client.create_tweet(text=text)
